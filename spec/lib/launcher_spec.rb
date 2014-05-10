f6891b5bf99ec928fe9ea3cd2c0b32f69a0c20e7
Adrien Folie <folie.adrien@gmail.com>
2014-05-11 00:14:34 +0200

    Update launcher_spec.rb

--- spec/lib/launcher_spec.rb
+++ spec/lib/launcher_spec.rb
@@ -19,53 +19,51 @@ describe Launcher do
     launcher.launch
   end
 
-	it 'should start a game' do
-		launcher.start
-		expect(launcher.game.started?).to be_true
-	end
+  it 'should start a game' do
+    launcher.start
+    expect(launcher.game.started?).to be_true
+  end
 
   it 'should play if game is started' do
     launcher.start
     expect(launcher.play?).to be_true
   end
  
-	it 'should run a game until this end' do
-		game = Game.new(Board.new(3), Computer.new(Params::X_SYMBOL), Computer.new(Params::O_SYMBOL))
-		
-		launcher.set(game)
-		launcher.start
-		launcher.run
-		expect(launcher.play?).to be_false
-	end
-
-	context 'when game is running' do
-
-		it 'should display' do
-			launcher.stubs(:play)
-    	launcher.stubs(:display)
-
-			launcher.stubs(:play?).returns(true, false)
-    	launcher.expects(:display)
-    	launcher.run
-		end
-
-	end
+  it 'should run a game until this end' do
+    game = Game.new(Board.new(3), Computer.new(Params::X_SYMBOL), Computer.new(Params::O_SYMBOL))
 	
-	context 'when displaying' do
-		module OutputHelper
-			def self.print_board(*args) ; end
-			def self.print_state(*args) ; end
-		end
+    launcher.set(game)
+    launcher.start
+    launcher.run
+    expect(launcher.play?).to be_false
+  end
 
-		it 'should print board' do
-			OutputHelper.expects(:print_board).with(launcher.game.board)
-			launcher.display
-		end
+  context 'when game is running' do
 
-		it 'should print state' do
-			OutputHelper.expects(:print_state).with(launcher.game.state)
-			launcher.display
-		end
-	end
+    it 'should display' do
+      launcher.stubs(:play)
+      launcher.stubs(:display)
+      launcher.stubs(:play?).returns(true, false)
+      launcher.expects(:display)
+      launcher.run
+    end
+  end
 
+  context 'when displaying' do
+    module OutputHelper
+      def self.print_board(*args) ; end
+      def self.print_state(*args) ; end
+    end
+
+    it 'should print board' do
+      OutputHelper.expects(:print_board).with(launcher.game.board)
+      launcher.display
+    end
+    
+    it 'should print state' do
+      OutputHelper.expects(:print_state).with(launcher.game.state)
+      launcher.display
+    end
+  end
+  
 end
